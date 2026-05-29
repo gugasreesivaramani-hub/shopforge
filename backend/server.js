29e40619b1f0e7df52b01497946a5ba1cc75b3dd
ShopForge User <no-reply@shopforge.local>
2026-05-29 20:07:07 +0530

    remove frontend dist serving

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,7 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
 
-    if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
-      app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-      });
-    }
+
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
